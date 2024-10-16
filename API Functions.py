f3751a35587f00a736e73fe667665b724f88633d
mouryachandan <138875295+mouryachandan@users.noreply.github.com>
2024-10-16 16:11:05 +0530

    Add files via upload

---
+++ API Functions.py
@@ -0,0 +1,10 @@
+def create_rule(rule_string):
+    return root_node  
+
+
+
+def combine_rules(rules):
+    return combined_root_node  
+
+def evaluate_rule(ast, data):
+    return evaluation_result  
---
+++ AST Data Structure Node Class.py
@@ -0,0 +1,7 @@
+
+class Node:
+    def __init__(self, node_type, left=None, right=None, value=None):
+        self.type = node_type 
+        self.left = left  
+        self.right = right  
+        self.value = value 
